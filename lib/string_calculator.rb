a26b7698e40c0452b79b2fdc5104f7c77c1cb72c
Vivek R <45197313+VRVivekR@users.noreply.github.com>
2024-03-23 12:46:18 +0530

    when string is empty

---
+++ lib/string_calculator.rb
@@ -0,0 +1,7 @@
+class StringCalculator
+    class << self
+        def add(string)
+            return 0 if string.empty?
+        end
+    end
+end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,2 +1,15 @@
+require "string_calculator"
+
 describe StringCalculator do
+    describe ".add" do
+        let(:subject) { StringCalculator }
+        
+        context "when string is empty" do
+            string = ""
+
+            it "returns 0" do
+                expect(subject.add(string)).to eq 0
+            end
+        end
+    end
 end
